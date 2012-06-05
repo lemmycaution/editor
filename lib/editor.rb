a85741f2e1f6f8dd31a6783691d95378a895ca94
Onur Uyar <fluxproject@gmail.com>
2012-06-05 13:12:53 +0100

    some fixes

--- lib/editor.rb
+++ lib/editor.rb
@@ -8,6 +8,12 @@ module Editor
   @@use_suffix = true
   mattr_accessor :use_suffix
   
+  @@has_visual_editor = true
+  mattr_accessor :has_visual_editor  
+  
+  @@locales_count = true
+  mattr_accessor :locales_count  
+  
   def self.setup
     yield self
   end
